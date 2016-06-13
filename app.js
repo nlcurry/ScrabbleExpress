2315288bc74a5855a38b1d84f2245dcae5d50062
Nadine Curry <nlcurry@gmail.com>
2016-06-13 15:55:28 -0700

    installed express and nodemon

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
 var app = express();
 
 // view engine setup
@@ -19,8 +22,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-var indexRoutes = require('./routes/index');
-app.use('/', indexRoutes);
+app.use('/', routes);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
