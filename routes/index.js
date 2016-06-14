8f7b7d527c624cf9c13bdbc00efa42b5bbd6b07c
Nadine Curry <nlcurry@gmail.com>
2016-06-14 09:09:19 -0700

    finished routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var Player = require('../lib/player.js')
+var Scrabble = require('../lib/scrabble.js')
+var Score = require('../controllers/score')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Scrabble EXPRESS!' });
 });
+var scrabble = new Scrabble ()
+
+router.get('/scrabble/chart', function(req,res) {
+  res.render('chart', {scrabble: scrabble._score})
+});
+
+router.get('/scrabble/score', Score.getWord);
+router.post('/scrabble/score', Score.getScore);
 
 module.exports = router;
